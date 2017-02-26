4d97e11e3698619a358d856efe935e23e62c1eae
Pedro Azevedo <pedro.azevedo@concrete.com.br>
2017-02-26 18:35:29 -0300

    Added alexa-jenkins-api.

---
+++ jenkins-api/src/index.js
@@ -0,0 +1,46 @@
+var APP_ID = undefined;
+
+var AlexaSkill = require('./AlexaSkill');
+var jenkinsapi = require ('jenkins-api');
+
+var jenkins = jenkinsapi.init(process.env.AUTH_URL);
+
+var Fact = function () {
+	AlexaSkill.call(this, APP_ID);
+};
+
+Fact.prototype = Object.create(AlexaSkill.prototype);
+Fact.prototype.constructor = Fact;
+
+Fact.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+	handleNewFactRequest(response);
+};
+
+Fact.prototype.intentHandlers = {
+	"GetJenkinsIntent": function (intent, session, response) {
+		handleNewFactRequest(response);
+	},
+	"AMAZON.HelpIntent": function (intent, session, response) {
+		response.ask("Ask who is the person that agrees.");
+	}
+};
+
+function handleNewFactRequest(response) {
+	return jenkins.all_jobs(function(err, data){
+		var jobsJ = [];
+		for (var i=0; i<data.length; i++){
+			jobsJ.push(data[i].name);
+		}
+
+		var speechOutput = "The jobs found are:  " + jobsJ.join(', and ,');
+		var cardTitle = "Your Answer";
+		response.tellWithCard(speechOutput, cardTitle, speechOutput);
+	});
+
+
+}
+// Create the handler that responds to the Alexa Request.
+exports.handler = function (event, context) {
+	var fact = new Fact();
+	fact.execute(event, context);
+};

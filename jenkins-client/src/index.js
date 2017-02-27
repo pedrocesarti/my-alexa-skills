e9253cf9a7c042a689b4b7a807d7d074d1f4f8f0
Pedro Azevedo <pedro.azevedo@concrete.com.br>
2017-02-27 11:43:15 -0300

    Added function to build.

--- jenkins-client/src/index.js
+++ jenkins-client/src/index.js
@@ -13,19 +13,31 @@ Fact.prototype = Object.create(AlexaSkill.prototype);
 Fact.prototype.constructor = Fact;
 
 Fact.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-	handleNewFactRequest(response);
+	handleListJobs(response);
 };
 
 Fact.prototype.intentHandlers = {
-	"GetJenkinsIntent": function (intent, session, response) {
-		handleNewFactRequest(response);
+	"ListJobsJenkinsIntent": function (intent, session, response) {
+		handleListJobs(response);
+	},
+	"BuildJobJenkinsIntent": function (intent, session, response) {
+		handleBuildJob(response);
 	},
 	"AMAZON.HelpIntent": function (intent, session, response) {
-		response.ask("Ask who is the person that agrees.");
+		response.ask("Ask if letisea agrees with Pedro.");
+	},
+	"AMAZON.StopIntent": function (intent, session, response) {
+		var speechOutput = "Goodbye";
+		response.tell(speechOutput);
+	},
+	"AMAZON.CancelIntent": function (intent, session, response) {
+		var speechOutput = "Goodbye";
+		response.tell(speechOutput);
 	}
 };
 
-function handleNewFactRequest(response) {
+// List jobs
+function handleListJobs(response) {
 	return jenkins.job.list(function(err, data){
 		var jobsJ = [];
 		for (var i=0; i<data.length; i++){
@@ -33,14 +45,27 @@ function handleNewFactRequest(response) {
 		}
 
 		var speechOutput = "The jobs found are:  " + jobsJ.join(', and ,');
-		var cardTitle = "Your Answer";
+		var cardTitle = "Return!";
 		response.tellWithCard(speechOutput, cardTitle, speechOutput);
 	});
-
-
 }
+
+// Build jobs
+function handleBuildJob(response){
+	var speechOutput;
+	var cardTitle;
+
+	return jenkins.job.build('test', function(err, data){
+		speechOutput = "Success starting job."
+		if (err){speechOutput = "Error starting job."}
+		cardTitle = "Return!";
+		response.tellWithCard(speechOutput, cardTitle, speechOutput);
+	}
+})
+
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
 	var fact = new Fact();
 	fact.execute(event, context);
 };
+
